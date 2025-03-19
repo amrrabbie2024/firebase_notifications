fb2aa9615d585907732ff4d6a83aede78a31c70b
amr-rabbie <amrrabbie@yahoo.com>
2025-03-19 13:00:12 +0200

    amr

--- lib/core/logic/firebase_notifications.dart
+++ lib/core/logic/firebase_notifications.dart
@@ -14,6 +14,7 @@ class FirebaseNotifications{
      await _firebaseMessaging.requestPermission();
      String? token=await _firebaseMessaging.getToken();
      print("Token $token");
+     handleBackgroundNotification();
    }
 
 
